e807c8e0081f6c6cc46b361eac187be631ccc0a7
Boeun's <rnqhdms@gmail.com>
2023-09-06 23:01:09 +0900

    TomcatServletWebServerFactory를 사용한 서버 기동

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -2,12 +2,19 @@ package com.tobyspring.helloboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
 @SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HellobootApplication.class, args);
+
+        //스프링 부트가 지원하는 'TomcatServletWebServerFactory'를 사용하면 톰캣 웹 서버(서블릿 컨테이너)를 실행하는 코드를 만들 수 있다.
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
+
     }
 
 }
