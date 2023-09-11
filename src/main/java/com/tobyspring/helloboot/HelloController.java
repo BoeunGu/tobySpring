b32a2fbf16bef941e82cca3c9281f86999e4489a
Boeun's <rnqhdms@gmail.com>
2023-09-12 00:52:55 +0900

    Controller와 Service의 역할 분리

--- src/main/java/com/tobyspring/helloboot/HelloController.java
+++ src/main/java/com/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,14 @@
 package com.tobyspring.helloboot;
 
+import java.util.Objects;
+
 public class HelloController {
     public String hello(String name) {
-        return "Hello" + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        //HelloController와 SimpleHelloService의 역할 분리
+        // HelloController : 사용자의 요청이 유효한지 검증
+        // SimpleHelloService : 실제 비즈니스 로직 처리
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
