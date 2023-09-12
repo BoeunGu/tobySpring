8c83e13c25392db6b526a6a4138281683dfd3fc5
Boeun's <rnqhdms@gmail.com>
2023-09-13 02:20:19 +0900

    DispatcherServlet으로의 전환

--- src/main/java/com/tobyspring/helloboot/HelloController.java
+++ src/main/java/com/tobyspring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package com.tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,8 +15,13 @@ public class HelloController {
         this.helloService = helloService;
     } // 생성자 주입
 
-    public String hello(String name) {
 
+    @GetMapping("/hello")
+    @ResponseBody
+    // @GetMapping 애노테이션 : DispatcherServlet이 web 요청을 처리할 수 있는 클래스로 인식하고 매핑 테이블에 저장한다.
+    // @ResponseBody 애노테이션 : String을 리턴하는 컨트롤러를 DispatcherServlet이 처리하는 가장 기본적인 방법은, 해당 이름의 view(html template)를 찾는 것이다.
+    // 해당 애노테이션을 사용하면 리턴 값을 웹 응답의 body에 넣에 text/plain 형태로 응답하게 해준다.
+    public String hello(String name) {
         //HelloController와 SimpleHelloService의 역할 분리
         // HelloController : 사용자의 요청이 유효한지 검증
         // SimpleHelloService : 실제 비즈니스 로직 처리
--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -4,23 +4,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
 
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class); // bean 등록
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh(); //컨테이너에 필요한 정보를 등록하고 refresh()를 이용해서 초기화 작업을 진행한다.
@@ -31,29 +26,8 @@ public class HellobootApplication {
             @Override
             public void onStartup(ServletContext servletContext) throws ServletException {
 
-
-                //서블릿 등록
-                servletContext.addServlet("frontController", new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                        // 인증, 보안, 다국어 등 공통 기능을 Front Controller가 담당하도록 함.
-                        if (req.getRequestURI().equals("/hello")
-                                && req.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = req.getParameter("name");
-
-                            HelloController helloController = applicationContext.getBean(HelloController.class);
-                            String ret = helloController.hello(name);
-
-                            resp.setContentType("Text/plain");
-                            resp.getWriter().println(ret);
-                        } else if (req.getRequestURI().equals("/user")) {
-                            //
-                        } else {
-                            resp.setStatus(HttpStatus.NOT_FOUND.value());
-                        }
-
-                    }
-                }).addMapping("/*"); //FrontController 역할을 담당하도록 모든 요청을 처리하게 한다.
+                servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
+                        .addMapping("/*"); //FrontController 역할을 담당하도록 모든 요청을 처리하게 한다.
             }
         });
         webServer.start();
