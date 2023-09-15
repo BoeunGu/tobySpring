5c75296e397a8c3e746859f4ab39a69847d85eb3
Boeun's <rnqhdms@gmail.com>
2023-09-15 22:55:42 +0900

    Spring Container 초기화 할때 - Servelt 등록

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -15,22 +15,27 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() { //스프링 컨테이너를 초기화 할때 서블릿컨테이너를 만들고 서블릿을 초기화 하는 작업을 수행 하도록 변경
+                super.onRefresh();
+                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
+                    @Override
+                    public void onStartup(ServletContext servletContext) throws ServletException {
+
+                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+                                .addMapping("/*"); //FrontController 역할을 담당하도록 모든 요청을 처리하게 한다.
+                    }
+                });
+                webServer.start();
+
+            }
+        };
         applicationContext.registerBean(HelloController.class); // bean 등록
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh(); //컨테이너에 필요한 정보를 등록하고 refresh()를 이용해서 초기화 작업을 진행한다.
 
-        //스프링 부트가 지원하는 'TomcatServletWebServerFactory'를 사용하면 톰캣 웹 서버(서블릿 컨테이너)를 실행하는 코드를 만들 수 있다.
-        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
-            @Override
-            public void onStartup(ServletContext servletContext) throws ServletException {
-
-                servletContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext))
-                        .addMapping("/*"); //FrontController 역할을 담당하도록 모든 요청을 처리하게 한다.
-            }
-        });
-        webServer.start();
 
     }
 
