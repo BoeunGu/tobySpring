83f8ff9b5977eb7e30ec13d68c177100b5baab59
Boeun's <rnqhdms@gmail.com>
2023-09-18 00:59:14 +0900

    자바코드를 이용한 Spring Container 구성 정보 사용

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -1,21 +1,23 @@
 package com.tobyspring.helloboot;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-@SpringBootApplication
+@Configuration // 해당 애노테이션이 붙은 클래스가 AnnotationConfigApplicationContext에 처음 등록되는 빈 이다.
 public class HellobootApplication {
 
     public static void main(String[] args) {
 
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+        // 자바 코드를 이용한 구성 정보를 사용하려면 AnnotationConfigApplicationContext 클래스로 컨테이너를 만들어야한다.
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext() {
             @Override
             protected void onRefresh() { //스프링 컨테이너를 초기화 할때 서블릿컨테이너를 만들고 서블릿을 초기화 하는 작업을 수행 하도록 변경
                 super.onRefresh();
@@ -32,11 +34,20 @@ public class HellobootApplication {
 
             }
         };
-        applicationContext.registerBean(HelloController.class); // bean 등록
-        applicationContext.registerBean(SimpleHelloService.class); // bean 등록
+        applicationContext.register(HellobootApplication.class);
         applicationContext.refresh(); //컨테이너에 필요한 정보를 등록하고 refresh()를 이용해서 초기화 작업을 진행한다.
 
 
+    }
+
+    @Bean // 스프링 컨테이너에게 bean 등록에 사용될 factory method라고 알려주는 역할.
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
+
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
     }
 
 }
