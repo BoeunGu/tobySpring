e8d4a0c75489a6f8ed4a9ebfb363927078d8dd91
Boeun's <rnqhdms@gmail.com>
2023-09-19 23:45:52 +0900

    DispatcherServlet의 초기화 : ApplicationContextAware 구현

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -3,7 +3,9 @@ package com.tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -22,12 +24,15 @@ public class HellobootApplication {
             @Override
             protected void onRefresh() { //스프링 컨테이너를 초기화 할때 서블릿컨테이너를 만들고 서블릿을 초기화 하는 작업을 수행 하도록 변경
                 super.onRefresh();
-                TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this); //
                 WebServer webServer = serverFactory.getWebServer(new ServletContextInitializer() {
                     @Override
                     public void onStartup(ServletContext servletContext) throws ServletException {
 
-                        servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this))
+                        servletContext.addServlet("dispatcherServlet", dispatcherServlet)
                                 .addMapping("/*"); //FrontController 역할을 담당하도록 모든 요청을 처리하게 한다.
                     }
                 });
@@ -39,5 +44,15 @@ public class HellobootApplication {
         applicationContext.refresh(); //컨테이너에 필요한 정보를 등록하고 refresh()를 이용해서 초기화 작업을 진행한다.
 
 
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
     }
 }
