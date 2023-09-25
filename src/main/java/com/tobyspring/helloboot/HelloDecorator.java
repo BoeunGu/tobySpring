6e306d90af436c56a70c5f551938c387972530aa
Boeun's <rnqhdms@gmail.com>
2023-09-25 23:56:33 +0900

    Decorateor Pattern

--- src/main/java/com/tobyspring/helloboot/HelloDecorator.java
+++ src/main/java/com/tobyspring/helloboot/HelloDecorator.java
@@ -1,8 +1,10 @@
 package com.tobyspring.helloboot;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // DI시 해당 빈을 우선적으로 주입 함.
 public class HelloDecorator implements HelloService {
     //데코레이터 패턴
     private final HelloService helloService;
