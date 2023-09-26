a1343a44f97540599cdd40b17b624b44963ac5ea
Boeun's <rnqhdms@gmail.com>
2023-09-26 23:51:29 +0900

    Meta Annotation

--- src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
+++ src/test/java/com/tobyspring/helloboot/HelloServiceTest.java
@@ -2,11 +2,25 @@ package com.tobyspring.helloboot;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Test
+@interface UnitTest {
+// @Test를 확장한 Meta Annotation
+}
 
 public class HelloServiceTest {
 
-    @Test
+    //Unit Test
+    @UnitTest
     void simpleHelloService() {
 
         SimpleHelloService simpleHelloService = new SimpleHelloService();
