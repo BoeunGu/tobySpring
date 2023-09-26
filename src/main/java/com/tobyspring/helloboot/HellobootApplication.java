fbb36290f278bcb8432bd53faccbd8f5de195c50
Boeun's <rnqhdms@gmail.com>
2023-09-27 00:07:16 +0900

    MySpringBootAnnotation을 활용한 @SpringBootAnnotation 대체

--- src/main/java/com/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/com/tobyspring/helloboot/HellobootApplication.java
@@ -3,12 +3,9 @@ package com.tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration // 해당 애노테이션이 붙은 클래스가 AnnotationConfigApplicationContext에 처음 등록되는 빈 이다.
-@ComponentScan // 해당 클래스가 있는 패키지 부터 하위 패키지 까지 @Component 애노테이션이 붙어있는 클래스들을 빈으로 등록시켜 줌.
+@MySpringBootAnnotation
 public class HellobootApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -0,0 +1,17 @@
+package com.tobyspring.helloboot;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME) // 기존 자바 애노테이션의 RetentionPolicy는 class인데,
+// 즉, 애노테이션 정보가 컴파일도니 클래스 파일까지는 살아 있지만, 런타임 로딩시에는 정보가 사라지므로, 해당 애노테이션의 정보를 유지하기 위해서 RetentionPolicy를 런타임으로 지정.
+@Target(ElementType.TYPE) // TYPE은 클래스, 인터페이서, 이넘에 해당 함.
+@Configuration // 해당 애노테이션이 붙은 클래스가 AnnotationConfigApplicationContext에 처음 등록되는 빈 이다.
+@ComponentScan // 해당 클래스가 있는 패키지 부터 하위 패키지 까지 @Component 애노테이션이 붙어있는 클래스들을 빈으로 등록시켜 줌.
+public @interface MySpringBootAnnotation {
+}
