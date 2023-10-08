86f9e63cb062b918fc558231f59794faa4a7a99c
Boeun's <rnqhdms@gmail.com>
2023-10-08 23:32:13 +0900

    자동 구성 정보 파일 분리

---
+++ src/main/java/com/tobyspring/config/autoConfig/MyAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.tobyspring.config.autoConfig;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Configuration // MyAutoConfiguration을 사용하면 @Configuration 클래스로 선언한 것과 동일한 효과를 나타냄
+public @interface MyAutoConfiguration {
+
+}
