ff0a77a11d3833a07cc28a9bc5c7fc062061f727
Boeun's <rnqhdms@gmail.com>
2023-10-16 23:35:02 +0900

    @Contional 추가

--- src/main/java/com/tobyspring/config/autoConfig/TomcatWebServerConfig.java
+++ src/main/java/com/tobyspring/config/autoConfig/TomcatWebServerConfig.java
@@ -8,7 +8,8 @@ import org.springframework.context.annotation.Bean;
 @MyAutoConfiguration
 public class TomcatWebServerConfig {
 
-    @Bean
+    @Bean("tomcatWebServerFactory")
+
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
