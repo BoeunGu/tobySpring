a78f44b687495ca437205af957cd57779c0bd5c3
Boeun's <rnqhdms@gmail.com>
2023-10-24 22:13:41 +0900

    ConditionalMyOnClass 애노테이션 적용하여 서블릿 컨테이너 선택

--- src/main/java/com/tobyspring/config/ConditionalMyOnClass.java
+++ src/main/java/com/tobyspring/config/ConditionalMyOnClass.java
@@ -7,6 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+//@Conditional 애노테이션을 확장하여 만든 Meta Annotation
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Conditional(MyOnClassCondition.class)
